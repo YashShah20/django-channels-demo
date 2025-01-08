e99ae85cea5fecf1da5e6e001a422bf30227bc5d
Yash Shah <yshah@argusoft.com>
2025-01-08 23:11:49 +0530

    minor change

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,7 +75,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("red-ctvbgphopnds739e1ui0", 6379)],
         },
     },
 }
